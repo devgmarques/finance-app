edb9adba6c196430426558756b6e16aa72962d14
Guilherme <guilhermehenrique12308@gmail.com>
2024-10-23 20:47:51 -0300

    chore: initial nativecn setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/Card"
+import { Text } from "react-native"
 
 export default function Index() {
   return (
-    <View className="bg-black">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Card>
+      <CardHeader>
+        <CardTitle>Accelerate UI</CardTitle>
+        <CardDescription>Enter a new development experience</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Text className="text-base text-primary">
+          Sleek, easy to use components to build your next app faster.
+        </Text>
+      </CardContent>
+      <CardFooter>
+        <Text className="text-sm text-muted-foreground">
+          Inspired by shadcn/ui
+        </Text>
+      </CardFooter>
+    </Card>
+
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-
-    },
+    extend: {},
   },
   plugins: [],
 }
